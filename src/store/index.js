b6d7fc39574552d57a2d2dd83b3670b3ea1a7374
shivesh-12 <shiveshr9@gmail.com>
2022-03-31 23:03:33 +0530

    5 files modified

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
+import uiSlice from './ui-slice';
+const store = configureStore({
+  reducer: { ui: uiSlice.reducer },
+});
 
-
+export default store;
