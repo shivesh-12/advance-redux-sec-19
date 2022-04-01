6147f630148985382c9213e444b858e58e635d74
shivesh-12 <shiveshr9@gmail.com>
2022-04-01 23:59:25 +0530

    1 file modified

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -4,13 +4,21 @@ const cart = createSlice({
   name: 'cart',
   initialState: {
     items: [],
-    itemQuantity: 0,
+    itemQuantity: 0, 
   },
   reducers: {
     addItemToCart(state, action) {
       const newItem = action.payload;
-      const existingItem =  state.item.find(item => item.id === newItem.id);
-      
+      const existingItem = state.item.find((item) => item.id === newItem.id);
+      if (!existingItem) {
+        state.item.push({
+          itemId: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title,
+        });
+      }
     },
     removeItemFromCart(state) {},
   },
