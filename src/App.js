ef8db88db54b991c85d7f27c2a918ab92c6a6aae
shivesh-12 <shiveshr9@gmail.com>
2022-04-19 07:15:08 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { useSelector, useDispatch } from 'react-redux';
+import { uiActions } from './store/ui-slice';
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart); // use selector sets up a subscription to redux so whenever redux store dose change this will re-execute when cart is change.
 
   useEffect(() => {
     const sendCartData = async () => {
+      dispatch(uiA);
       const response = await fetch(
         'https://react-redux-19-default-rtdb.firebaseio.com/cart.json',
         {
