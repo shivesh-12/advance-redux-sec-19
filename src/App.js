68bdb5153b41edaa1fe919ba501f24b727e55320
shivesh-12 <shiveshr9@gmail.com>
2022-04-19 06:49:20 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { useSelector } from 'react-redux';
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch('https://react-redux-19-default-rtdb.firebaseio.com/cart.json');
+  }, []);
+
   return (
     <Layout>
-      {showCart && <Cart />} 
+      {showCart && <Cart />}
       <Products />
     </Layout>
   );
