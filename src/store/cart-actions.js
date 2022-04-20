747d2e1e80f3737659d08bc685f6f226b43e8024
shivesh-12 <shiveshr9@gmail.com>
2022-04-20 07:32:24 +0530

    2 files modified

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -1,3 +1,36 @@
+import { uiActions } from './ui-slice';
+import { cartActions } from './cart-slice';
+
+export const fetchCartData = () => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+      const response = await fetch(
+        'https://react-redux-19-default-rtdb.firebaseio.com/cart.json'
+      );
+
+      if (!response.ok) {
+        throw new Error('Could not fetch cart data!');
+      }
+
+      const data = await response.json();
+      return data;
+    };
+
+    try {
+      const cartData = await fetchData();
+      dispatch(cartActions.replaceCart(cartData));
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error!',
+          message: 'Fetching cart data failed!',
+        })
+      );
+    }
+  };
+};
+
 export const sendCartData = (cart) => {
   return async (dispatch) => {
     dispatch(
@@ -13,7 +46,10 @@ export const sendCartData = (cart) => {
         'https://react-redux-19-default-rtdb.firebaseio.com/cart.json',
         {
           method: 'PUT',
-          body: JSON.stringify(cart),
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
         }
       );
 
